216ccf85a5854818304bf4866b39204a41da9a23
avidcoder123 <umar.tli.edu@gmail.com>
2021-03-20 02:55:17 +0000

    Edit setup script

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ mkdir tmp
 touch tmp/db.sqlite3
 node ace migration:run
 cat .env.example >> .env
-echo "Remember to fill in data for .env"
-echo "App key:"
-node ace generate:key
 node -r @adonisjs/assembler/build/register japaFile.ts
 git config --global --unset credential.helper
 git config credential.helper store
+echo "Remember to fill in data for .env"
+echo "App key:"
+node ace generate:key
