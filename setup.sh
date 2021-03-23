b5ea6f42c3e31fd6b72d605808370eb88999f445
avidcoder123 <umar.tli.edu@gmail.com>
2021-03-23 19:03:36 +0000

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
+cat .env.example >> .env
 npm install
-cd frontend
-npm install
-cd ..
+#Save the following scripts for when I start making frontend
+#cd frontend
+#npm install
+#cd ..
 mkdir tmp
 touch tmp/db.sqlite3
 node ace migration:run
-cat .env.example >> .env
 git config --global --unset credential.helper
 git config credential.helper store
-echo "Remember to fill in data for .env"
-echo "App key:"
-node ace generate:key
