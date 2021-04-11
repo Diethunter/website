3262060bb158226341dfcdba54dfaaebbafd9cb7
avidcoder123 <umar.tli.edu@gmail.com>
2021-04-11 02:00:51 +0000

    Create test file

---
+++ test/recipe.spec.ts
@@ -0,0 +1,9 @@
+import test from 'japa'
+import supertest from 'supertest'
+import Recipe from 'App/Models/Recipe'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group("Test Recipes", async function() {
+
+})
