2f51afb3ff9f5182be3a448f3798b68711e0655b
avidcoder123 <umar.tli.edu@gmail.com>
2021-04-12 18:23:39 +0000

    /workspace/ricetta/commit

--- commit.js
+++ commit.js
@@ -1,3 +1,3 @@
 const { exec } = require('child_process');
 
-exec("git add .", ()=> exec("git commit -m \""+process.argv[1]+"\"", () => exec("git push", ()=>{})))
+exec("git add .", (a, out)=> {console.log(out);exec("git commit -m \""+process.argv[1]+"\"", (a, out) => {console.log(out);exec("git push", (a, out)=>{console.log(out)})})})
