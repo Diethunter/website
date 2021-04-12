8591f0a5fcbf7f0bcaa5a014b55bbb54b82f4661
avidcoder123 <umar.tli.edu@gmail.com>
2021-04-12 18:24:26 +0000

    Delete commit script

--- commit.js
+++
@@ -1,3 +0,0 @@
-const { exec } = require('child_process');
-
-exec("git add .", (a, out)=> {console.log(out);exec("git commit -m \""+process.argv[1]+"\"", (a, out) => {console.log(out);exec("git push", (a, out)=>{console.log(out)})})})
