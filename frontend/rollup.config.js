d2843954d788b741bdfcf887aa5dd12b357e7928
avidcoder123 <umar.tli.edu@gmail.com>
2021-04-20 21:56:07 +0000

    Edit rollup config

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -71,7 +71,10 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload({
+            watch: 'public',
+            clientUrl: process.env.CLIENT_URL
+        }),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
